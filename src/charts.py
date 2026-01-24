6de856a612c7dd542427d50f8fab54fa33dd3ced
Byounghyo Lim <byounghyo.lim@gmail.com>
2026-01-24 21:37:17 +0900

    Update files

--- src/charts.py
+++ src/charts.py
@@ -309,7 +309,7 @@ def create_signal_category_table(
     fig.update_layout(
         title=dict(text=title, font=dict(size=16)),
         margin=dict(l=10, r=10, t=40, b=10),
-        #height=100 + max_len * 40,
+        height=100 + max_len * 40,
     )
 
     return fig
